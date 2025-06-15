6b6facd692589f5a46327081632113c782233379
YS-Raptor <yashan.sumanaratne@gmail.com>
2025-06-16 06:44:13 +1000

    Store measurements in micro seconds

--- src/debug.zig
+++ src/debug.zig
@@ -4,12 +4,12 @@ pub const Timer = struct {
     time: i64,
 
     pub fn start() @This() {
-        return Timer{ .time = std.time.milliTimestamp() };
+        return Timer{ .time = std.time.microTimestamp() };
     }
 
     pub fn timestamp(self: *@This(), message: []const u8) void {
-        const current_time = std.time.milliTimestamp();
-        std.debug.print("Timestamp: {s}, Elapsed: {d} ms\n", .{
+        const current_time = std.time.microTimestamp();
+        std.debug.print("Timestamp: {s}, Elapsed: {d} ns\n", .{
             message,
             current_time - self.time,
         });
